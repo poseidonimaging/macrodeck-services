6f21b3a7c30606d681a274a35f05d9883c216812
Keith Gable <ziggy@ignition-project.com>
2008-06-13 03:38:09 +0000

    Hopefully the problem with sorting is that events happening at the same time were causing a collision where the event replaced over the other events, causing what appeared to be duplicates, which were in actuality just the first event occuring at a specific time overwriting all others occuring at the same time.
    git-svn-id: http://svn.macrodeck.com/macrodeck-services/trunk@1143 22037474-b016-0410-8482-8abe5be14c89

--- lib/event_service/calendar_sorting.rb
+++ lib/event_service/calendar_sorting.rb
@@ -12,24 +12,26 @@ module EventServiceCommon
 	                event_times = []
 	                events_by_time = {}
 	                events_before_sort.each do |event|
-	                        event_times << event.start_time
-	                        events_by_time[event.start_time] = event
+							keyed_start_time = [event.start_time][event.id]
+	                        event_times << keyed_start_time 
+	                        events_by_time["#{event.start_time}-#{event.id}"] = event
 	                end
 	                # Sort the events now
 	                event_times.sort!
 	                # Now build the result
 	                events_after_sort = []
 	                event_times.each do |event_time|
-	                  # if the event ends now or later than now, show it if they requested, or show all if they didn't
-	                        if	(ignore_past_entries && 
-						!events_by_time[event_time].end_time.nil? &&		# if there is an end time and we ignore past entries
-						events_by_time[event_time].end_time >= Time.now) || 
-					(ignore_past_entries &&
-						events_by_time[event_time].end_time.nil? &&		# if there is no end time and we ignore past entries
-						event_time >= Time.now) ||
-					!ignore_past_entries						# if we ignore past entries
-					# if any of the above three conditions are met, add the event.
-	                                events_after_sort << events_by_time[event_time]
+					# if the event ends now or later than now, show it if they requested, or show all if they didn't
+						key = "#{event_time[0]}-#{event_time[1]}"
+						if	(ignore_past_entries && 
+								!events_by_time[key].end_time.nil? &&	# if there is an end time and we ignore past entries
+								events_by_time[key].end_time >= Time.now) || 
+							(ignore_past_entries &&
+								events_by_time[key].end_time.nil? &&	# if there is no end time and we ignore past entries
+								event_time[0] >= Time.now) ||
+							!ignore_past_entries						# if we don't ignore past entries
+							# if any of the above three conditions are met, add the event.
+	                                events_after_sort << events_by_time[key]
 	                        end
 	                end
 	                # Now return the result
