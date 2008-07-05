850c35c12b6ea42aba0250e750c40a2aeda5bd8e
Keith Gable <ziggy@ignition-project.com>
2008-07-05 17:56:09 +0000

    - More place metadata to correspond with ChefMoz
    git-svn-id: http://svn.macrodeck.com/macrodeck-services/trunk@1154 22037474-b016-0410-8482-8abe5be14c89

--- lib/places_service/place_metadata.rb
+++ lib/places_service/place_metadata.rb
@@ -1,7 +1,7 @@
 # PlaceMetadata is a class that we store as the objectdata of an item that contains all of the information you would want to know about a place.
 class PlaceMetadata
 	# Standard information
-	attr_accessor :address, :zipcode, :phone_number, :website, :latitude, :longitude, :type, :type_other, :features
+	attr_accessor :address, :zipcode, :phone_number, :website, :latitude, :longitude, :type, :type_other, :features, :parking
 
 	# Hours of operation
 	attr_accessor :hours_sunday, :hours_monday, :hours_tuesday, :hours_wednesday, :hours_thursday, :hours_friday, :hours_saturday
@@ -21,10 +21,12 @@ class PlaceMetadata
 						:takeout => "Takeout", # Same as carryout
 						:delivery => "Delivery",
 						:call_ahead => "Call-Ahead Seating",
+						:drive_thru => "Drive-Thru",
 						# Restaurant meals offered
 						:breakfast => "Breakfast",
 						:lunch => "Lunch",
 						:dinner => "Dinner",
+						:catering => "Catering",
 						# Atmosphere
 						:pets_allowed => "Pets Allowed",
 						:televisions => "Televisions",
@@ -32,6 +34,13 @@ class PlaceMetadata
 						:vegeterian_friendly => "Vegeterian-Friendly",
 						:live_music => "Live Music",
 						:date_friendly => "Date-Friendly",
+						:kid_friendly => "Kid Friendly",
+						:casual => "Casual",	# this is generically here because it can refer to atmosphere/dress
+						:formal_dress_required => "Formal Dress Required",
+						:smoking_permitted => "Smoking Permitted",
+						# Menus
+						:kids_menu => "Kids Menu",
+						:senior_menu => "Senior Menu",
 						# Money features
 						:cash_only => "Cash Only"
 	}
@@ -44,6 +53,15 @@ class PlaceMetadata
 					:bakery => "Bakery",
 					:public_place => "Public Place",
 					:other => "Other"
+	}
+	PLACE_PARKING = {	:own => "Own Parking Lot",
+						:valet => "Valet Parking",
+						:validated => "Validated Parking",
+						:street => "Street Parking",
+						:transit => "Public Transit Accessible",
+						:no_local => "No Local Parking",
+						:pay => "Pay Parking",
+						:parallel => "Parallel Parking"
 	}
 
 	def initialize
